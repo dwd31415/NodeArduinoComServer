82eb3139507a7373fd424bd24681817b69b5229d
Adrian Dawid <adriandwd@gmail.com>
2015-05-03 12:25:37 +0200

    Added node.js code to recieve messages over serial com

--- js/server_main.js
+++ js/server_main.js
@@ -2,3 +2,52 @@
  * Main Entry point to 		 *
  * Node.js server			 *
  *****************************/
+//Include type defs:
+/// <reference path="../typings/node/node.d.ts" />
+//Include libraries:
+var serialport = require("serialport");
+var http = require("http");
+var socketio = require('socket.io');
+/**
+  * A server, that sends all messages it recives over a serial port, to a callback function.
+ **/
+var ComInServer = (function () {
+    function ComInServer() {
+        ComInServer.currentInstance = this;
+    }
+    ComInServer.prototype.setCallback = function (newCallback) {
+        this.callback = newCallback;
+    };
+    ComInServer.prototype.setSerialPort = function (serialPort) {
+        this.serialPort = serialPort;
+        this.serialPort.on('data', function (data) {
+            ComInServer.currentInstance.callback(data);
+        });
+    };
+    return ComInServer;
+})();
+;
+function main(params) {
+    var currentPort = undefined;
+    serialport.list(function (err, ports) {
+        ports.forEach(function (port) {
+            console.log(port);
+            ports += port.comName + "\n";
+            console.log(port.comName);
+            if (port.comName === params[2]) {
+                currentPort = port;
+            }
+        });
+        var activeSerialPort = new serialport.SerialPort(currentPort.comName, {
+            baudrate: 9600
+        });
+        activeSerialPort.on("open", function () {
+            var comInServer = new ComInServer();
+            comInServer.setSerialPort(activeSerialPort);
+            comInServer.setCallback(function (data) {
+                console.log("" + data);
+            });
+        });
+    });
+}
+main(process.argv);
--- ts/server-main.ts
+++ ts/server-main.ts
@@ -7,11 +7,62 @@
 /// <reference path="../typings/node/node.d.ts" />
  
  //Include libraries:
- var serialPort = require("serialport");
+ var serialport = require("serialport");
  var http = require("http");
+ var socketio = require('socket.io');
+
+/**
+  * A server, that sends all messages it recives over a serial port, to a callback function.
+ **/
+class ComInServer
+{
+	serialPort : any;
+	callback: Function;
+		
+	static currentInstance : ComInServer;
+	
+	constructor()
+	{
+		ComInServer.currentInstance = this;
+	}
+	setCallback(newCallback:Function)
+	{
+		this.callback = newCallback;
+	}
+	setSerialPort(serialPort : any)
+	{
+		this.serialPort = serialPort;
+		this.serialPort.on('data', function(data) {
+			ComInServer.currentInstance.callback(data);
+		});
+	}
+};
  
- export module ArduinoCom
- {
-	 
- }
+ 
+ function main(params:string[]) {
+	let currentPort : any = undefined;
+ 	serialport.list(function (err, ports) {
+    	ports.forEach(function(port) {
+			console.log(port);
+        	ports += port.comName + "\n";
+        	console.log(port.comName);
+     		if(port.comName === params[2])
+    		{
+        		currentPort = port;
+      		}
+   	 });
+    var activeSerialPort = new serialport.SerialPort(currentPort.comName, {
+        baudrate: 9600
+    });
+    activeSerialPort.on("open", function () {
+      var comInServer = new ComInServer();
+	  comInServer.setSerialPort(activeSerialPort);
+	  comInServer.setCallback(function (data) {
+		  console.log();
+	  });
+    });
+  });
+}
+ 
+ main(process.argv);
  
